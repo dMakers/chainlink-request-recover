e5682546aeed3a0390ed5a2df07f3016c80ffe98
Mihael Dorn <micdorn@protonmail.com>
2020-01-25 01:32:47 -0800

    update

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
-## Chainlink node requests recover
-This set of scripts allows you to recover all the Oracle requests that have not been `fullfilled` by particular node.
+## Oracle request finder for chainlink node operator
+As a chainlink node operator, you may have missed a few job requests. This tool helps you to find those unfullfilled requests, so you can execute them.
+
+# How to run
+Prepare `.env` file
+
+| ENV_VAR | Description |
+| --- | --- |
+| ORACLE_ADDRESS | Oracle contract address |
+| START_BLOCK | When to start collecting events |
+| JOB_ID | Job id to look up |
+| NODE_ADDRESS | Whitelisted key on your Oracle address for validation |
+| FAKE_RESPONSE | Node operator response for unfullfiled job |
+| RPC_URL | Ethereum HTTP node RPC url |
+| BLOCK_INTERVAL | How many blocks to query in for loop |
+| PRIVATE_KEY | Private key (without 0x) to send a request to your Oracle contract - only needed if you wish to execute it |
+| GAS_PRICE | Gas price to use for sending txs in gwei |
 
 1. `npm i`
-1. Look thru the `./index.js` and change the `ORACLE_ADDRESS`, `ORACLE_CREATION_BLOCK`, `JOB_ID`, `NODE_ADDRESS` vars. In this particular case we use `CURRENT_ETHEREUM_PRICE` var as `data` for the request fullfilment. It depends on you Job.
-1. `node index.js` - it will be running a while and adding found unfulfilled requests to `txs` file.
-1. Look thru the `./submit.js` and change `ORACLE_ADDRESS`, `GAS_PRICE` and `PRIVATE_KEY` vars.
-1. `node submit.js` - it will try to fullfil all the found request (from `txs`) file.
+2. `npm run find-missing-requests` - starts searching for missing requests and writes it to `unfulfilled_requests` file
+3. `npm run fulfill-requests` - it will try to fulfill found requests from `unfulfilled_requests` file.
+
+
+Example:
+```
+Start processing blocks from 9229786 to 9249786
+We got 445 request events. Start processing...
+Request without fillfilment found! Blocknumber is 9247478
+Start processing blocks from 9249786 to 9269786
+```
--- submit.js
+++ submit.js
@@ -1,56 +1,60 @@
+require('dotenv').config()
 const Web3 = require('web3')
 const { numberToHex, toHex, toWei } = require('web3-utils')
 const fs = require('fs')
-
-const ORACLE_ADDRESS = '0x64fe692be4b42f4ac9d4617ab824e088350c11c2'
 const ORACLE_ABI = require('./oracle.abi.json')
-const PRIVATE_KEY = '' // CHANGE IT
-const GAS_PRICE = '3.2' // in Gwei
 
-const web3 = new Web3('https://mainnet.infura.io', null, { transactionConfirmationBlocks: 1 })
+const {
+  ORACLE_ADDRESS,
+  PRIVATE_KEY,
+  GAS_PRICE,
+  RPC_URL
+} = process.env
+
+const web3 = new Web3(RPC_URL, null, { transactionConfirmationBlocks: 1 })
 const account = web3.eth.accounts.privateKeyToAccount('0x' + PRIVATE_KEY)
 web3.eth.accounts.wallet.add('0x' + PRIVATE_KEY)
 web3.eth.defaultAccount = account.address
 const oracle = new web3.eth.Contract(ORACLE_ABI, ORACLE_ADDRESS)
 
 async function main() {
-    const txs = JSON.parse(fs.readFileSync('txs'))
-    // console.log('txs', txs)
-    let nonce = await web3.eth.getTransactionCount(account.address)
-    console.log('nonce', nonce)
-    for(let i = 0; i < txs.length; i++) {
-        const args = txs[i]
-        const data = oracle.methods.fulfillOracleRequest(...args).encodeABI()
-        try {
-            const gas = await oracle.methods.fulfillOracleRequest(...args).estimateGas()
-            const tx = {
-                from: web3.eth.defaultAccount,
-                value: '0x00',
-                gas: numberToHex(500000),
-                gasPrice: toHex(toWei(GAS_PRICE, 'gwei')),
-                to: oracle._address,
-                netId: 1,
-                data,
-                nonce
-            }
-            let signedTx = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY)
-            let result
-            if (i % 50 === 0) {
-                result = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
-                console.log(`A new successfully sent tx ${result}`)
-            } else {
-                result = web3.eth.sendSignedTransaction(signedTx.rawTransaction)
-                result.once('transactionHash', function(txHash){
-                    console.log(`A new successfully sent tx ${txHash}`)
-                }).on('error', async function(e){
-                    console.log('error', e.message)
-                })
-            }
-            nonce++
-            
-        } catch(e) {
-        }
+  const file = '[' + fs.readFileSync('unfulfilled_requests').slice(0, -2) + ']'
+  const txs = JSON.parse(file)
+  let nonce = await web3.eth.getTransactionCount(account.address)
+  console.log('nonce', nonce)
+  for(let i = 0; i < txs.length; i++) {
+    const args = txs[i]
+    const data = oracle.methods.fulfillOracleRequest(...args).encodeABI()
+    try {
+      const gas = await oracle.methods.fulfillOracleRequest(...args).estimateGas()
+      const tx = {
+        from: web3.eth.defaultAccount,
+        value: '0x00',
+        gas: numberToHex(500000), // please make sure its above `MINIMUM_CONSUMER_GAS_LIMIT` in your Oracle.sol
+        gasPrice: toHex(toWei(GAS_PRICE, 'gwei')),
+        to: oracle._address,
+        netId: 1,
+        data,
+        nonce
+      }
+      let signedTx = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY)
+      let result
+      if (i % 50 === 0) {
+        result = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+        console.log(`A new successfully sent tx ${result.transactionHash}`)
+      } else {
+        result = web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+        result.once('transactionHash', function(txHash){
+          console.log(`A new successfully sent tx ${txHash}`)
+        }).on('error', async function(e){
+          console.log('error', e.message)
+        })
+      }
+      nonce++
+    } catch(e) {
+      console.error('skipping tx ', txs[i], e)
     }
+  }
 }
 
 main()
